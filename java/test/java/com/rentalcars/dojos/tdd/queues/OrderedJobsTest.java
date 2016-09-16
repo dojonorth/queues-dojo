b8f3a40d0985ca7e006d295f2d4942f546f43a7c
murphyj <jmurphy84@googlemail.com>
2016-09-16 14:21:35 +0100

    First test for Queues

--- java/test/java/com/rentalcars/dojos/tdd/queues/OrderedJobsTest.java
+++ java/test/java/com/rentalcars/dojos/tdd/queues/OrderedJobsTest.java
@@ -3,8 +3,6 @@ package com.rentalcars.dojos.tdd.queues;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.util.ArrayList;
-
 import static org.junit.Assert.assertEquals;
 
 public class OrderedJobsTest {
@@ -17,43 +15,7 @@ public class OrderedJobsTest {
     }
 
     @Test
-    public void noSortedJobsReturnsEmptyString() {
+    public void sortedEmptyOrderedListReturnsEmpty() {
         assertEquals("", orderedJobs.sort());
     }
-
-    @Test
-    public void singleJobReturnsJobWhenSorted() {
-        orderedJobs.register('a');
-        assertEquals("a", orderedJobs.sort());
-    }
-
-    @Test
-    public void twoIndependentJobsWhenSortedReturnInTheOrderTheyWereRegistered() {
-        orderedJobs.register('a');
-        orderedJobs.register('b');
-        assertEquals("ab", orderedJobs.sort());
-    }
-
-    @Test
-    public void jobSortedBeforeDependentJob() {
-        orderedJobs.register('a', 'b');
-        assertEquals("ba", orderedJobs.sort());
-    }
-
-    @Test
-    public void twoJobsWithTwoDependencies() {
-        orderedJobs.register('b', 'a');
-        orderedJobs.register('c', 'd');
-        assertEquals("abdc", orderedJobs.sort());
-    }
-
-    @Test
-    public void dependencyChain() {
-        orderedJobs.register('a', 'b'); // b -> a
-        orderedJobs.register('b', 'c'); // c -> b
-        assertEquals("cba", orderedJobs.sort()); // c -> b - > a
-    }
-
-    // independents c
-    // dependents b a
 }
